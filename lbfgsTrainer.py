ff8a8225ff2daa47f078ed4976fb726b80d6b3af
Oneplus <oneplus.lau@gmail.com>
2012-09-22 08:34:02 +0800

    first commit

---
+++ lbfgsTrainer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import sys
+
+class lbfgsTrainer(object):
+    def __init__(self):
+        pass
+
+    def train(self, data):
+        pass
+
+
+if __name__=="__main__":
+    print >> sys.stderr, "library is not runnable."
